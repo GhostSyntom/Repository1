2511e4f511bd007a6d674d5d1bf3b138e026b037
GhostSyntom <159362627+GhostSyntom@users.noreply.github.com>
2025-11-18 16:38:43 +0200

    Add files via upload

---
+++ MVCDivisionWithouthLeftout/MVCDivisionWithouthLeftout/DivisionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCDivisionWithouthLeftout
+{
+    public class DivisionController
+    {
+        private DivisionModel _model;
+        public DivisionController()
+        {
+            _model = new DivisionModel();
+        }
+        public void GetInput()
+        {
+            Console.WriteLine("Enter n:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                _model.Numbers.Add(int.Parse(Console.ReadLine()));
+            }
+        }
+        public void DisplayResult()
+        {
+            Console.WriteLine($"{_model.PercentDivBy2:F2}%");
+            Console.WriteLine($"{_model.PercentDivBy3:F2}%");
+            Console.WriteLine($"{_model.PercentDivBy4:F2}%");
+        }
+    }
+}
---
+++ MVCDivisionWithouthLeftout/MVCDivisionWithouthLeftout/DivisionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCDivisionWithouthLeftout
+{
+    public class DivisionModel
+    {
+        public List<int> Numbers { get; set; } = new List<int>();
+        public double PercentDivBy2 => (double)Numbers.FindAll(x => x % 2 == 0).Count / Numbers.Count * 100;
+        public double PercentDivBy3 => (double)Numbers.FindAll(x => x % 3 == 0).Count / Numbers.Count * 100;
+        public double PercentDivBy4 => (double)Numbers.FindAll(x => x % 4 == 0).Count / Numbers.Count * 100;
+    }
+}

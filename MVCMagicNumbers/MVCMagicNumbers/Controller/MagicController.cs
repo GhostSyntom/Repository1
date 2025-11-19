6163d61e7f3638d5bb445312fcf705c14ed2f52d
GhostSyntom <ghostsyntom@gmail.com>
2025-11-19 10:57:39 +0200

    New folders and refactored code  and Net 9

---
+++ MVCMagicNumbers/MVCMagicNumbers/Controller/MagicController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCMagicNumbers
+{
+    public class MagicController
+    {
+        private MagicModel _model;
+        public MagicController()
+        {
+            _model = new MagicModel();
+        }
+        public void GetInput()
+        {
+            Console.WriteLine("Enter magic number:");
+            _model.MagicNumber = int.Parse(Console.ReadLine());
+        }
+        public void Process()
+        {
+            _model.GenerateMagicNumbers();
+        }
+        public void DisplayResult()
+        {
+            Console.WriteLine(string.Join(" ", _model.MagicNumbers));
+        }
+    }
+}
---
+++ MVCMagicNumbers/MVCMagicNumbers/View/Program.cs
@@ -0,0 +1,12 @@
+﻿using MVCMagicNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MagicController controller = new MagicController();
+        controller.GetInput();
+        controller.Process();
+        controller.DisplayResult();
+    }
+}

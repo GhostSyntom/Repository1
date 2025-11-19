e9cbee8d8e64fb91db2b4bdda2de39823771cf06
GhostSyntom <ghostsyntom@gmail.com>
2025-11-19 10:48:30 +0200

    New folders and refactored code and Net 9

---
+++ MVCTubeForPool/MVCTubeForPool/Controller/PoolController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTubeForPool
+{
+    public class PoolController
+    {
+        private PoolModel _model;
+        public PoolController()
+        {
+            _model = new PoolModel();
+        }
+        public void GetInput()
+        {
+            Console.WriteLine("Enter pool volume V:");
+            _model.V = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter pipe 1 flow rate P1:");
+            _model.P1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter pipe 2 flow rate P2:");
+            _model.P2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter hours H:");
+            _model.H = double.Parse(Console.ReadLine());
+        }
+        public void DisplayResult()
+        {
+            if (_model.IsOverflow)
+            {
+                Console.WriteLine($"For {_model.H} hours the pool overflows with {_model.Overflow} liters.");
+            }
+            else
+            {
+                Console.WriteLine($"The pool is {_model.FullPercent}% full. Pipe 1: {_model.Pipe1Percent}%. Pipe 2: {_model.Pipe2Percent}%.");
+            }
+        }
+    }
+}
---
+++ MVCTubeForPool/MVCTubeForPool/Models/PoolModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTubeForPool
+{
+    public class PoolModel
+    {
+        public int V { get; set; }
+        public int P1 { get; set; }
+        public int P2 { get; set; }
+        public double H { get; set; }
+        public double TotalWater => (P1 + P2) * H;
+        public bool IsOverflow => TotalWater > V;
+        public int FullPercent => (int)((TotalWater / V) * 100);
+        public double WaterP1 => P1 * H;
+        public double WaterP2 => P2 * H;
+        public int Pipe1Percent => (int)((WaterP1 / TotalWater) * 100);
+        public int Pipe2Percent => (int)((WaterP2 / TotalWater) * 100);
+        public double Overflow => TotalWater - V;
+    }
+}

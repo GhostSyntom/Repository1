6fdccd9458ae5e0d30afca4ecb77a430e64531d5
GhostSyntom <ghostsyntom@gmail.com>
2025-11-19 11:08:58 +0200

    New folders and refactored code  and Net 9

---
+++ MVCTileRepair/MVCTileRepair/Controller/TileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTileRepair
+{
+    public class TileController
+    {
+        private TileModel _model;
+        public TileController()
+        {
+            _model = new TileModel();
+        }
+        public void GetInput()
+        {
+            _model.N = double.Parse(Console.ReadLine());
+            _model.W = double.Parse(Console.ReadLine());
+            _model.L = double.Parse(Console.ReadLine());
+            _model.M = double.Parse(Console.ReadLine());
+            _model.O = double.Parse(Console.ReadLine());
+        }
+        public void Process()
+        {
+            _model.Calculate();
+        }
+        public void DisplayResult()
+        {
+            Console.WriteLine(_model.TilesNeeded);
+            Console.WriteLine(_model.Time);
+        }
+    }
+}
---
+++ MVCTileRepair/MVCTileRepair/Models/TileModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTileRepair
+{
+    public class TileModel
+    {
+        public double N { get; set; }
+        public double W { get; set; }
+        public double L { get; set; }
+        public double M { get; set; }
+        public double O { get; set; }
+        public double TilesNeeded { get; set; }
+        public double Time { get; set; }
+        public void Calculate()
+        {
+            double area = N * N;
+            double bench = M * O;
+            double toCover = area - bench;
+            double tile = W * L;
+            TilesNeeded = toCover / tile;
+            Time = TilesNeeded * 0.2;
+        }
+    }
+}

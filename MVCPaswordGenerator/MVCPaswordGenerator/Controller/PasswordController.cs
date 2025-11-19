fde9bc337ad44c61a0bf45229d841b4f96435765
GhostSyntom <ghostsyntom@gmail.com>
2025-11-19 10:20:47 +0200

    New folders and refactored code

---
+++ MVCPaswordGenerator/MVCPaswordGenerator/Controller/PasswordController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCPaswordGenerator
+{
+    public class PasswordController
+    {
+        private PasswordModel _model;
+        private PasswordView _view;
+
+        public PasswordController()
+        {
+            _model = new PasswordModel();
+            _view = new PasswordView();
+        }
+
+        public void Run()
+        {
+            _model.N = _view.ReadInt("Enter n: ");
+            _model.L = _view.ReadInt("Enter l: ");
+
+            var passwords = _model.GeneratePasswords();
+
+            _view.ShowPasswords(passwords);
+        }
+    }
+}

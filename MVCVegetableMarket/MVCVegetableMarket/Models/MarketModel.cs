0de85e60bb240fe9e234e951336c2dae7d2d0d78
GhostSyntom <ghostsyntom@gmail.com>
2025-11-19 10:44:54 +0200

    New folders and refactored code  and Net 9

---
+++ MVCVegetableMarket/MVCVegetableMarket/Models/MarketModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MVCVegetableMarket
+{
+    internal class MarketModel
+    {
+        public double LevPerVegetableKg;
+        public double LevPerFruitKg;
+
+        public double LevToEuroRate = 1.94;
+
+        public int TotalVegetableKg;
+        public int TotalFruitKg;
+
+        public double CalculateEuro()
+        {
+            double totalLeva = (LevPerVegetableKg * TotalVegetableKg) + (LevPerFruitKg * TotalFruitKg);
+            return totalLeva / 1.94;
+        }
+        
+    }
+}

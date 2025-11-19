59d42d8140080bfb74b6cdfaf77ec1c18cfb602f
GhostSyntom <ghostsyntom@gmail.com>
2025-11-19 10:02:59 +0200

    New folders were made, The dot.Net was changed to 9 and the code was refactored.

---
+++ MVCPriceForTicket/MVCPriceForTicket/Controller/TransportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCPriceForTicket
+{
+    public class TransportController
+    {
+        private TransportView _view;
+        private TransportModel _model;
+
+        public TransportController()
+        {
+            _view = new TransportView();
+            _model = new TransportModel();
+        }
+
+        public void Run()
+        {
+            _model.Kilometers = _view.ReadKilometers();
+            _model.DayOrNight = _view.ReadDayOrNight();
+
+            double cheapest = _model.GetCheapestTransport();
+
+            _view.ShowResult(cheapest);
+        }
+    }
+}
